4d59cef9fdb2576774c95aaa0cdf7a56ac095558
MyHP <ducle140503@gmail.com>
2024-04-25 03:26:56 +0700

    11

--- Project/MNHP/src/main/java/Model/User.java
+++ Project/MNHP/src/main/java/Model/User.java
@@ -1,16 +1,17 @@
-//package Model;
-//
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.ToString;
-//
-//import java.util.Date;
-//@Getter
-//@Builder
-//@ToString
-//
-//public class User extends CBNV {
-//    public User(String idCBNV, String hoten, String idChucVu, String chucVu, String noiSinh, String queQuan, String diaChiTT, String SDT, String email, Date ngayVaoLam, String soCCCD, String idTinhTrang, String tinhTrang) {
-//        super(idCBNV, hoten, idChucVu, chucVu, noiSinh, queQuan, diaChiTT, SDT, email, ngayVaoLam, soCCCD, idTinhTrang, tinhTrang);
-//    }
-//}
+package Model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+@Setter
+@Getter
+@ToString
+public class User extends CBNV {
+    public User(String idCBNV, String hoten, String idChucVu, String chucVu, String noiSinh, String queQuan, String diaChiTT, String SDT, String email, Date ngayVaoLam, String soCCCD, String idTinhTrang, String tinhTrang) {
+        super(idCBNV, hoten, idChucVu, chucVu, noiSinh, queQuan, diaChiTT, SDT, email, ngayVaoLam, soCCCD, idTinhTrang, tinhTrang);
+    }
+
+}
